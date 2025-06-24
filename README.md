04937f2ad50509c3ec2f519d3c9a2cdd2f27c02d
Erick Weil <erick.weil@ifro.edu.br>
2025-06-23 21:56:17 -0400

    Preparando para publicar no npm

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# estruturas-ts
+# Estruturas-TS
 
+Estruturas de dados em TypeScript.
+
+Estruturas criadas:
+- ArrayStack: Pilha baseada em array
+- LinkedStack: Pilha utilizando referências simplesmente ligada
+- ArrayQueue: Fila baseada em array circula
+- DualStackQueue: Fila baseada em duas pilhas
+
+> Este pacote é experimental, utilize por sua conta e risco.
+
+## Utilização:
+
+Veja a pasta [./exemplos](./exemplos/) para exemplos de utilização.
---
+++ index.ts
@@ -0,0 +1,13 @@
+// utils
+export { ConsumerQueue } from "./src/utils/consumerQueue.js";
+
+// interfaces
+export { Queue } from "./src/interfaces/queue.js";
+export { Stack } from "./src/interfaces/stack.js";
+export { Deque } from "./src/interfaces/deque.js";
+
+// estruturas
+export { LinkedStack } from "./src/estruturas/linkedStack.js";
+export { DualStackQueue } from "./src/estruturas/dualStackQueue.js";
+export { ArrayStack } from "./src/estruturas/arrayStack.js";
+export { ArrayQueue } from "./src/estruturas/arrayQueue.js";
