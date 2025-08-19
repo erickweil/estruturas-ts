bc82c15b902be9d19a81cd02b5081b443df6d9d2
Erick Weil <erick.weil@ifro.edu.br>
2025-08-18 22:25:02 -0400

    Adicionado bubble sort

---
+++ src/ordenacao/bubbleSort.ts
@@ -0,0 +1,22 @@
+export const bubbleSort = (arr: number[]) => {
+    while(true) {
+        let trocas = 0;
+        // 1. Percorrer cada par do array
+        for(let i = 0; i < arr.length - 1; i++) {
+            let a = arr[i];
+            let b = arr[i+1];
+
+            // 2. Não Está em ordem? troca!
+            if(a > b) {
+                // está errado tem que trocar.
+                arr[i] = b;
+                arr[i+1] = a;
+                trocas++;
+            }
+        }
+        if(trocas == 0) {
+            break;
+        }
+    }   
+    return arr;
+};
---
+++ test/ordenacao/ordem.spec.ts
@@ -0,0 +1,36 @@
+import { jest, describe, expect, test, afterAll, beforeAll } from "@jest/globals";
+import { bubbleSort } from "../../src/ordenacao/bubbleSort.js";
+
+describe("Testes em Ordenação", () => {
+
+    function gerarLista(tamanho: number) {
+        const lista = [];
+        for(let i = 0; i < tamanho; i ++) {
+            lista.push(Math.floor(Math.random() * tamanho));
+        }
+        return lista;
+    }
+
+    function verificaOrdem(lista: number[]) {
+        let anterior = lista[0];
+        for(let elemento of lista) {
+            expect(elemento >= anterior).toBe(true);
+            anterior = elemento;
+        }
+    }
+
+    test("BubbleSort", () => {
+        let listas = [
+            [9,8,7,6,5,4,3,2,1,0],
+            [0,1,2,3,4,5,6,7,8,9],
+            [7,4,8,2,8,0,1,7,8,9],
+            gerarLista(10),
+            gerarLista(100),
+            gerarLista(1000)
+        ]
+        for(let lista of listas) {
+            lista = bubbleSort(lista);
+            verificaOrdem(lista);
+        }
+    });
+});
