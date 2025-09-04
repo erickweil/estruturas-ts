aafc00d516c59a8dfbde9b6b95266dbb9314f822
Erick Weil <erick.weil@ifro.edu.br>
2025-09-04 16:20:29 -0400

    Adicionado código ao index.ts para poder ser usado como um pacote

--- index.ts
+++ index.ts
@@ -1,13 +1,27 @@
 // utils
 export { ConsumerQueue } from "./src/utils/consumerQueue.js";
+export * as structSchema from "./src/utils/structSchema.js";
 
 // interfaces
+export { Deque } from "./src/interfaces/deque.js";
+export { List } from "./src/interfaces/list.js";
 export { Queue } from "./src/interfaces/queue.js";
 export { Stack } from "./src/interfaces/stack.js";
-export { Deque } from "./src/interfaces/deque.js";
 
 // estruturas
-export { LinkedStack } from "./src/estruturas/linkedStack.js";
-export { DualStackQueue } from "./src/estruturas/dualStackQueue.js";
-export { ArrayStack } from "./src/estruturas/arrayStack.js";
+export { ArrayDeque } from "./src/estruturas/arrayDeque.js";
 export { ArrayQueue } from "./src/estruturas/arrayQueue.js";
+export { ArrayStack } from "./src/estruturas/arrayStack.js";
+export { BufferPool } from "./src/estruturas/bufferPool.js";
+export { DualStackQueue } from "./src/estruturas/dualStackQueue.js";
+export { LinkedList } from "./src/estruturas/linkedList.js";
+export { LinkedStack } from "./src/estruturas/linkedStack.js";
+export { PoolList } from "./src/estruturas/poolList.js";
+
+// busca
+export { binarySearch, binarySearchLeftMost } from "./src/algoritmos/busca/binarySearch.js";
+
+// ordenação
+export { bubbleSort } from "./src/algoritmos/ordenacao/bubbleSort.js";
+export { binaryInsertionSort, insertionSort } from "./src/algoritmos/ordenacao/insertionSort.js";
+export { selectionSort } from "./src/algoritmos/ordenacao/selectionSort.js";
